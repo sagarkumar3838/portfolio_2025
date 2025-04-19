07125b4bd2e714d5503dad515109eb169d585f97
sagarkumar3838 <sagarsamal3838@gmail.com>
2025-04-19 10:23:59 +0530

    error deploy

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -4,7 +4,8 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faX } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-scroll';
 
-import { NAVIGATION_LINKS } from "/constants/Index.jsx";
+import { NAVIGATION_LINKS } from "../constants/Index";
+
 
 const NavBar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
